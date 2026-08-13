8a65e7e6a0c6a7fdd7433483571de5d1722cb3dd
Ashraf200012 <ashrafsayyad071@gmail.com>
2026-08-13 21:06:28 +0530

    Update server.py

--- server.py
+++ server.py
@@ -2,8 +2,22 @@ import http.server
 import os
 
 class MyCGIHandler(http.server.CGIHTTPRequestHandler):
-    # Allow CGI scripts to be run from the root directory instead of just cgi-bin
     cgi_directories = ['/']
+
+    def is_cgi(self):
+        # Redirect the main website URL to home.py
+        if self.path == '/':
+            self.path = '/home.py'
+            
+        # Run the default check to set up the script path
+        is_script = super().is_cgi()
+        
+        # ONLY run it as a CGI script if it is a .py file
+        if is_script and self.path.split('?')[0].endswith('.py'):
+            return True
+            
+        # For all other files (css, images, html), serve them normally
+        return False
 
 PORT = int(os.environ.get("PORT", 8080))
 print(f"Starting CGI server on port {PORT}...")
